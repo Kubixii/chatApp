93455b35ee214bc3c1f6dc0b067da824b5f3da6f
Jakub Filipek <39913230+Kubixii@users.noreply.github.com>
2023-01-21 18:29:39 +0100

    animated typing indicator

--- Client/src/components/TypingIndicator/TypingIndicator.jsx
+++ Client/src/components/TypingIndicator/TypingIndicator.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import bemCssModules from 'bem-css-modules'
 import { default as typingindicatorStyles } from './TypingIndicator.module.scss'
+import { useEffect } from 'react';
 
 const style = bemCssModules(typingindicatorStyles)
 
-const TypingIndicator = ({ who }) => {
-    return (
+const TypingIndicator = ({ who, isTyping }) => {
+
+    const [show, setShow] = useState(isTyping)
+    const cssClass = isTyping ? "slideIn" : "slideOut"
+
+    useEffect(() => {
+        if (isTyping) setShow(true)
+    }, [isTyping])
+
+    const handleAnimationEnd = () => {
+        if (!isTyping) setShow(false)
+    }
+
+    return show && (
         <div className={style()}>
-            <p>{who} is typing<span className={style('dots')}></span></p>
+            <p
+                className={`${style('text')} ${style(cssClass)}`}
+                onAnimationEnd={handleAnimationEnd}
+            >
+                {who} is typing<span className={style('dots')} />
+            </p>
         </div>
     );
 }
